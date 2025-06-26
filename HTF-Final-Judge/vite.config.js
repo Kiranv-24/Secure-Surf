c36f963b37eda7777de7c95cc1af37e10891751a
KIRAN V <151758693+Kiranv-24@users.noreply.github.com>
2025-06-26 21:42:27 +0530

    Update vite.config.js

--- HTF-Final-Judge/vite.config.js
+++ HTF-Final-Judge/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-  },
+ server: {
+  host: '0.0.0.0',
+  port: process.env.PORT || 8080,
+},
   root: './',
   build: {
     outDir: 'dist',
